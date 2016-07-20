0124e60de4e99ecca4902f4590ee279cea1e3888
rayk216 <raykoren@gmail.com>
2016-07-20 16:22:57 -0600

    ng click

--- app.js
+++ app.js
@@ -40,3 +40,11 @@ var app = angular.module("myapp", []);
       $scope.secondsInACentury = (60 * 60 * 24 * 365 * 100);
       $scope.rN = Date.now();
     });
+app.controller("click", function($scope){
+  $scope.view = {};
+  $scope.view.number = 5;
+  $scope.pickRandomNumber = function() {
+  $scope.view.number = Math.floor(Math.random() * 10) + 1;
+};
+
+});
