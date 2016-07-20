f12542fd3b75fddf2a08e174a479a3298570d1c3
rayk216 <raykoren@gmail.com>
2016-07-20 12:17:43 -0600

    camera exercise

--- app.js
+++ app.js
@@ -1,17 +1,39 @@
 angular.module("myapp", [])
-      .controller("HelloController", function($scope) {
+    .controller("HelloController", function($scope) {
         $scope.hello = {};
         $scope.hello.title = "World";
 
-      }).controller("symbols", function($scope) {
-              $scope.symbols = ['Spades', 'Clubs', 'Hearts', 'Diamonds'];
-              $scope.symbols.title = "Big";
-            });
-            $scope.overSix = function(value) {
+    }).controller("symbols", function($scope) {
+        $scope.symbols = ['Spades', 'Clubs', 'Hearts', 'Diamonds'];
+        $scope.symbols.title = "Big";
+    }).controller("cameras", function($scope) {
+        $scope.cameras = [{
+            title: 'Nikon D3100 DSLR',
+            image: 'http://ecx.images-amazon.com/images/I/713u2gDQqML._SX522_.jpg',
+            rating: 3.4,
+            price: 369.99,
+            onSale: true
+        }, {
+            title: 'Canon EOS 70D',
+            image: 'http://ecx.images-amazon.com/images/I/81U00AkAUWL._SX522_.jpg',
+            rating: 2.0,
+            price: 1099.0,
+            onSale: false
+        }, {
+            title: 'Nikon D810A',
+            image: 'http://ecx.images-amazon.com/images/I/91wtXIfLl2L._SX522_.jpg',
+            rating: 4.2,
+            price: 3796.95,
+            onSale: true
+        }];
 
-            if (value < 6)
-              return false;
-            else
-              return true;
+    });
 
-            };
+$scope.overSix = function(value) {
+
+    if (value < 6)
+        return false;
+    else
+        return true;
+
+};
